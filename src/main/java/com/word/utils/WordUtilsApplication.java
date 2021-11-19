4e0b300ad89d3fb16668c137c2d3ae5b91ba5695
cyf <chenyunfei@inman.cc>
2021-11-19 15:07:17 +0800

    first commit

---
+++ src/main/java/com/word/utils/WordUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.word.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordUtilsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/word/utils/wordutils/WordUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.word.utils.wordutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
