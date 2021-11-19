cd300b2d665206fbebaf40104377a862fe0fde87
cyf <chenyunfei@inman.cc>
2021-11-19 17:11:47 +0800

    [v1]word模板插入图片

--- src/main/java/com/word/utils/demo/WordUtils.java
+++ src/main/java/com/word/utils/demo/WordUtils.java
@@ -1,11 +1,12 @@
 package com.word.utils.demo;
 
 import org.apache.poi.ooxml.POIXMLDocument;
+import org.apache.poi.util.Units;
+import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -26,19 +27,19 @@ public class WordUtils {
 
         String inputSrc = "C:\\Users\\Administrator.DESKTOP-EFOTAD8\\Desktop\\test.docx";
         String outputSrc =  "C:\\Users\\Administrator.DESKTOP-EFOTAD8\\Desktop\\test02.docx";
-        Map<String, String> textMaps = new HashMap<>();
-        textMaps.put("${name}","chen");
-        copyWord(inputSrc,outputSrc,textMaps);
+        Map<String, String> contentMaps = new HashMap<>();
+        contentMaps.put("${name}","chen");
+        contentMaps.put("&img","C:\\Users\\Administrator.DESKTOP-EFOTAD8\\Desktop\\img.jpg");
+        copyWord(inputSrc,outputSrc,contentMaps);
 
     }
 
-    private static boolean copyWord(String inputSrc, String outputSrc, Map<String, String> textMaps) {
+    private static boolean copyWord(String inputSrc, String outputSrc, Map<String, String> contentMaps) {
 
         boolean success = true;
         try {
             XWPFDocument document = new XWPFDocument(POIXMLDocument.openPackage(inputSrc));
-            copyText(document,textMaps);
-
+            copyText(document,contentMaps);
             FileOutputStream outputStream = new FileOutputStream(outputSrc);
             document.write(outputStream);
             outputStream.close();
@@ -52,20 +53,48 @@ public class WordUtils {
 
     }
 
-    private static void copyText(XWPFDocument document, Map<String, String> textMaps) {
+    private static void copyText(XWPFDocument document, Map<String, String> contentMaps) {
         //获取 word 所有的段落
         List<XWPFParagraph> paragraphList = document.getParagraphs();
         for (XWPFParagraph paragraph : paragraphList) {
             List<XWPFRun> runs = paragraph.getRuns();
             for (XWPFRun run : runs) {
                 // run 一个单词
-                if (checkText(run.toString())){
+                String str = run.toString();
+                if (checkText(str)){
                     //包含 $则进行替换
                     //pos:位置，如果不要这个参数则是追加字符串
-                    run.setText(textMaps.get(run.toString()),0);
+                    run.setText(contentMaps.get(str),0);
+                }else if (checkImg(str)){
+                    setImg(run,paragraph,contentMaps);
                 }
 
             }
+        }
+    }
+
+    private static boolean checkImg(String str) {
+        if (str.contains("&")){
+            return true;
+        }
+        return false;
+    }
+
+    public static void setImg(XWPFRun run,XWPFParagraph paragraph,Map<String, String> contentMaps){
+        String str = run.toString();
+        try {
+            if ("&img".equals(str)){
+                String imgSrc = contentMaps.get(str);
+                FileInputStream is = new FileInputStream(imgSrc);
+                run.setBold(true);
+                paragraph.setAlignment(ParagraphAlignment.CENTER);
+                run.addBreak();
+                run.addPicture(is,XWPFDocument.PICTURE_TYPE_JPEG,imgSrc, Units.toEMU(200),Units.toEMU(200));
+                is.close();
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 
